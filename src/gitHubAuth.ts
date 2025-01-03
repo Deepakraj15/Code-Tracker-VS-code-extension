1afce0c3a2eaa395b2ab3850d879fddd83a07b71
Deepakraj15 <deepakraj15112002@gmail.com>
2025-01-03 07:06:19 +0530

    made some changes

--- src/gitHubAuth.ts
+++ src/gitHubAuth.ts
@@ -1,14 +1,14 @@
 import  * as vscode from 'vscode';
 import 'dotenv/config';
 
-export const getGitHubAuthSession = async() =>{
+export const getGitHubAuthSession = async () => {
     const GITHUB = "github";
-    const scopes =['repo, user:email','delete_repo'];
-    try{
-        const session = await vscode.authentication.getSession(GITHUB,scopes);
+    const scopes = ['repo, user:email', 'delete_repo'];
+    try {
+        const session = await vscode.authentication.getSession(GITHUB, scopes, { createIfNone: true });
         return session;
     }
-    catch(error){
+    catch (error) {
         vscode.window.showErrorMessage('GitHub authentication failed: ' + (error as Error).message);
     }
-}
+};
